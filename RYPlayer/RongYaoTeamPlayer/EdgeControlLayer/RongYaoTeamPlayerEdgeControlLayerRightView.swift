0c0453fa5144a93ff35fbd46581487ec60ada0f3
changsanjiang <changsanjiang@gmail.com>
2018-06-24 11:31:02 +0800

    EdgeControlLayer - `添加 EdgeControlLayer`

---
+++ RYPlayer/RongYaoTeamPlayer/EdgeControlLayer/RongYaoTeamPlayerEdgeControlLayerRightView.swift
@@ -0,0 +1,9 @@
+//
+//  RongYaoTeamPlayerEdgeControlLayerRightView.swift
+//  RongYaoTeamPlayer
+//
+//  Created by 畅三江 on 2018/6/24.
+//  Copyright © 2018年 SanJiang. All rights reserved.
+//
+
+import Foundation
---
+++ RYPlayer/RongYaoTeamPlayer/EdgeControlLayer/RongYaoTeamPlayerEdgeControlLayerTopView.swift
@@ -0,0 +1,9 @@
+//
+//  RongYaoTeamPlayerEdgeControlLayerTopView.swift
+//  RongYaoTeamPlayer
+//
+//  Created by 畅三江 on 2018/6/24.
+//  Copyright © 2018年 SanJiang. All rights reserved.
+//
+
+import Foundation
